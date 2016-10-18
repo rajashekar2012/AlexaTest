a17bcfb30ea734f18f65640765f1995c07176a71
rajashekar <rajashekarkhallel@gmail.com>
2016-10-18 17:43:04 +0530

    added alexa response json

--- app.js
+++ app.js
@@ -9,8 +9,54 @@ app.get('/', function (req, res) {
 
 // This responds a POST request for the homepage
 app.post('/', function (req, res) {
-   console.log("Got a POST request for the homepage");
-   res.send('Hello POST');
+   //console.log("Got a POST request for the homepage");
+   //res.send('Hello POST');
+  var response= {
+  "version": "string",
+  "sessionAttributes": {
+    "string": object
+  },
+  "response": {
+    "outputSpeech": {
+      "type": "string",
+      "text": "string",
+      "ssml": "string"
+    },
+    "card": {
+      "type": "string",
+      "title": "string",
+      "content": "string",
+      "text": "string",
+      "image": {
+        "smallImageUrl": "string",
+        "largeImageUrl": "string"
+      }
+    },
+    "reprompt": {
+      "outputSpeech": {
+        "type": "string",
+        "text": "string",
+        "ssml": "string"
+      }
+    },
+    "directives": [
+      {
+        "type": "string",
+        "playBehavior": "string",
+        "audioItem": {
+          "stream": {
+            "token": "string",
+            "url": "string",
+            "offsetInMilliseconds": 0
+          }
+        }
+      }
+    ],
+    "shouldEndSession": boolean
+  }
+}
+
+res.send(response);
 })
 
 // This responds a DELETE request for the /del_user page.
@@ -22,30 +68,8 @@ app.delete('/del_user', function (req, res) {
 // This responds a GET request for the /list_user page.
 app.get('/list_user', function (req, res) {
 	
-	 var response={
-	outputSpeech: {
-		type: "PlainText",
-		text: "Hello World Get"
-	},
-	card: {
-		type: "Simple",
-		title: "sample title",
-		content: "content"
-	},
-	repromt: {
-		outputSpeech: {
-			type: "PlainText",
-			text: "hello world repromt Get"
-		}
-	},
-	shouldEndSession: true
-}
-	 
-	 console.log("Got a GET request for /list_user");
-	 res.send(response);
-	
-   //console.log("Got a GET request for /list_user");
-  // res.send('Page Listing');
+  console.log("Got a GET request for /list_user");
+   res.send('Page Listing');
 })
 
 // This responds a GET request for abcd, abxcd, ab123cd, and so on
